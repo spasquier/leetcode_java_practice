15b559b51bbb7eac134907600cb7a4256b757b6c
Salvador Pasquier <admin@salvadorpasquier.com>
2026-04-01 16:01:42 -0600

    Change number to test solution to problem 1342

--- leetcode/solutions/Main.java
+++ leetcode/solutions/Main.java
@@ -3,7 +3,7 @@ package leetcode.solutions;
 class Main {
     public static void main(String[] args) {
         Solution1342 solution = new Solution1342();
-        int number = 0;
+        int number = 14;
         int result = solution.numberOfStepsToReduceNumberToZero(number);
         System.out.println(result);
     }
